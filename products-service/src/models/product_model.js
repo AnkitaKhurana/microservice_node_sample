66c781b2e4db34cdc49105cec85ef495ee8622bf
Ankita Khurana <Ankita.Khurana@netent.com>
2019-12-07 15:21:53 +0530

    Dockerfile

--- products-service/src/models/product_model.js
+++ products-service/src/models/product_model.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
 
 const ProductScema = new mongoose.Schema({
-
     name: String,
     type: { type: String, default: "item" },
     createdAt: { type: Date, default: Date.now },
     quantity: { type: Number, default: 1 },
     cost: { type: Number, default: 0 }
-
 });
 
 module.exports = mongoose.model("Product", ProductScema);
--- products-service/src/routes/product_routes.js
+++ products-service/src/routes/product_routes.js
@@ -5,22 +5,21 @@ const router = Router()
 router.get("/", async (req, res) => {
 
     const products = await Product.find({});
-
     try {
         res.send(products);
     } catch (err) {
         res.status(500).send(err);
     }
-
-
-
 });
 
 router.post("/", async (req, res) => {
-    const product = req.body;
-    const newProduct = new Product({ name: product.name });
+    const newProduct = new Product({ name: req.body.name });
     const savedProduct = await newProduct.save();
-    console.log(savedProduct)
-    res.json(savedProduct);
+    try {
+        res.send(savedProduct);
+    } catch (err) {
+        res.status(500).send(err);
+    }
 });
+
 module.exports = router;
